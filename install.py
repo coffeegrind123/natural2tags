72dcaf09fb65e32159307fb2d0804d005b2a8005
coffeegrind123 <103264516+coffeegrind123@users.noreply.github.com>
2024-07-15 07:42:54 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,5 +1,5 @@
 import launch
 
-if not launch.is_installed("regex"):
+if not launch.is_installed("text2tags-lib"):
     print("Installing requirements for Prompt Formatter")
-    launch.run_pip("install regex", "support for variable lookbehind")
+    launch.run_pip("install text2tags-lib", "make it so we can do this")
