69e52b347584dea50f266acc03141a8b089e1a0e
coffeegrind123 <103264516+coffeegrind123@users.noreply.github.com>
2024-07-15 22:48:11 +0300

    Update format_ui.py

--- scripts/format_ui.py
+++ scripts/format_ui.py
@@ -1,5 +1,6 @@
 import unicodedata
 from text2tags import TaggerLlama
+
 import gradio as gr
 from modules import script_callbacks, scripts, shared
 
@@ -10,9 +11,11 @@ References
 """
 ui_prompts = []
 
+
 """
 Functions
 """
+
 
 def format_prompt(*prompts: list):
     ret = []
@@ -30,12 +33,6 @@ def format_prompt(*prompts: list):
 
     return ret
 
-def process_prompt(prompt):
-    if not prompt or prompt.strip() == "":
-        return ""
-    tags = model.predict_tags(prompt)
-    formatted_tags = ', '.join(tags)
-    return formatted_tags
 
 def on_before_component(component: gr.component, **kwargs: dict):
     if "elem_id" in kwargs:
@@ -55,29 +52,6 @@ def on_before_component(component: gr.component, **kwargs: dict):
         return None
     return None
 
-class ScriptProcessPrompts(scripts.Script):
-    def title(self):
-        return "Prompt Processor"
 
-    def show(self, is_img2img):
-        return scripts.AlwaysVisible
 
-    def process(self, p, *args):
-        p.all_prompts = [process_prompt(x) for x in p.all_prompts]
-        p.all_negative_prompts = [process_prompt(x) for x in p.all_negative_prompts]
-
-        p.main_prompt = process_prompt(p.main_prompt)
-        p.main_negative_prompt = process_prompt(p.main_negative_prompt)
-
-        if getattr(p, 'enable_hr', False):
-            p.all_hr_prompts = [process_prompt(x) for x in p.all_hr_prompts]
-            p.all_hr_negative_prompts = [process_prompt(x) for x in p.all_hr_negative_prompts]
-
-            p.hr_prompt = process_prompt(p.hr_prompt)
-            p.hr_negative_prompt = process_prompt(p.hr_negative_prompt)
-
-def before_token_counter(params: script_callbacks.BeforeTokenCounterParams):
-    params.prompt = process_prompt(params.prompt)
-
-script_callbacks.on_before_token_counter(before_token_counter)
 script_callbacks.on_before_component(on_before_component)
