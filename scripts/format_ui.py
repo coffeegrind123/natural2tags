85a585739d468837ac7f3d83660d104c1191a5bc
coffeegrind123 <103264516+coffeegrind123@users.noreply.github.com>
2024-07-15 08:06:26 +0300

    Update format_ui.py

--- scripts/format_ui.py
+++ scripts/format_ui.py
@@ -1,19 +1,19 @@
 import unicodedata
+
 import gradio as gr
-from modules import script_callbacks
-from text2tags import TaggerLlama
+from modules import script_callbacks, scripts, shared
 
-model = TaggerLlama()
 
-SPACE_COMMAS = True
-BRACKET2WEIGHT = True
-SPACE2UNDERSCORE = False
-IGNOREUNDERSCORES = True
-
+"""
+References
+"""
 ui_prompts = []
 
-def normalize_characters(data: str):
-    return unicodedata.normalize("NFKC", data)
+
+"""
+Functions
+"""
+
 
 def format_prompt(*prompts: list):
     ret = []
@@ -23,20 +23,32 @@ def format_prompt(*prompts: list):
             ret.append("")
             continue
 
-        prompt = normalize_characters(prompt)
+        prompt = model.predict_tags(prompt)
+        print(prompt)
 
-        # Use TaggerLlama to predict tags
-        tags = model.predict_tags(prompt)
-        ret.append(', '.join(tags))
+        ret.append(prompt)
 
     return ret
 
+
 def on_before_component(component: gr.component, **kwargs: dict):
-    if "elem_id" in kwargs and kwargs["elem_id"] == "paste":
-        with gr.Blocks(analytics_enabled=False) as ui_component:
-            button = gr.Button(value="🪄", elem_classes="tool", elem_id="format")
-            button.click(fn=format_prompt, inputs=ui_prompts, outputs=ui_prompts)
-            return ui_component
+    if "elem_id" in kwargs:
+        if kwargs["elem_id"] in [
+            "txt2img_prompt",
+            "txt2img_neg_prompt",
+            "img2img_prompt",
+            "img2img_neg_prompt",
+        ]:
+            ui_prompts.append(component)
+            return None
+        elif kwargs["elem_id"] == "paste":
+            with gr.Blocks(analytics_enabled=False) as ui_component:
+                button = gr.Button(value="🪄", elem_classes="tool", elem_id="format")
+                button.click(fn=format_prompt, inputs=ui_prompts, outputs=ui_prompts)
+                return ui_component
+        return None
     return None
+
+
 
 script_callbacks.on_before_component(on_before_component)
