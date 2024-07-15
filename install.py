727dcc6a69bddbc3cae96be63ab12fc142836678
coffeegrind123 <103264516+coffeegrind123@users.noreply.github.com>
2024-07-15 08:06:45 +0300

    Update install.py

--- install.py
+++ install.py
@@ -2,4 +2,4 @@ import launch
 
 if not launch.is_installed("text2tags-lib"):
     print("Installing requirements for Prompt Formatter")
-    launch.run_pip("install text2tags-lib", "make it so we can do this")
+    launch.run_pip("install text2tags-lib", "text2tags-lib")
